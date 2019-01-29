074fff1ac75c9de810bfab4e77f7f9cd9aafbbd9
Andrew Tedstone <a.j.tedstone@bristol.ac.uk>
2019-01-29 13:02:57 +0000

    updates to msrededge

--- predict_msrededge.py
+++ predict_msrededge.py
@@ -8,17 +8,33 @@ import georaster
 
 # Stats for classifier in use
 #Accuracy =  0.952380952381 F1_Score =  0.949801013523 Recall =  0.952380952381 Precision =  0.950284090909
-clf_RF = joblib.load('/scratch/UAV/clf_RF_RedEdge.pkl')
+""" For RedEdge_snicarsnow.pkl
+Accuracy =  0.940217391304 F1_Score =  0.934766392673 Recall =  0.940217391304 Precision =  0.942895709008
 
-fn_path = '/scratch/UAV/uav2017_common_grid_nc/'
+Feature Importances
+(relative importance of each feature (wavelength) for prediction)
+
+R475 0.233517596955
+R560 0.260483344995
+R668 0.20433026048
+R717 0.152076480941
+R840 0.149592316629
+"""
+clf_RF = joblib.load('/scratch/UAV/clf_RF_RedEdge_snicarsnow.pkl')
+
+fn_path = '/scratch/UAV/uav2017_commongrid_bandcorrect/'
 flights = {
-#'20170715':'uav_20170715_refl_5cm.nc'
-'20170717':'uav_20170717_refl_5cm.nc',
-'20170720':'uav_20170720_refl_5cm.nc',
-'20170721':'uav_20170721_refl_5cm.nc',
-'20170722':'uav_20170722_refl_5cm.nc',
-'20170723':'uav_20170723_refl_nolenscorr_5cm.nc',
-'20170724':'uav_20170724_refl_5cm_v2.nc'
+'20170715':'uav_20170715_refl_5cm_commongrid.nc',
+'20170720':'uav_20170720_refl_5cm_commongrid.nc',
+'20170721':'uav_20170721_refl_5cm_commongrid.nc',
+'20170722':'uav_20170722_refl_5cm_commongrid.nc',
+'20170723':'uav_20170723_refl_5cm_commongrid.nc',
+'20170724':'uav_20170724_refl_5cm_commongrid.nc'
+}
+
+fn_path = '/scratch/UAV/photoscan_outputs_2018/'
+flights = {
+	'20180724_PM':'uav_20180724_PM_refl.nc'
 }
 
 setup = False
@@ -36,8 +52,8 @@ for flight in flights:
 	uav['Band1'] -= 0.17
 	uav['Band2'] -= 0.18
 	uav['Band3'] -= 0.15
-	uav['Band4'] -= 0.2
-	uav['Band5'] -= 0.05
+	uav['Band4'] -= 0.16
+	uav['Band5'] -= 0.1
 
 	b_ix = pd.Index([1,2,3,4,5],name='b') 
 
@@ -46,22 +62,24 @@ for flight in flights:
 	concat = concat.where(concat.sum(dim='b') > 0)
 
 	predicted = xarray_classify.classify_dataset(concat, clf_RF)
+
 	# Just look at a subset area in this case (slice)
 	#predicted = xarray_classify.classify_dataset(concat.isel(x=slice(3000,3500),y=slice(3000,3500)), clf_RF)
 
 	# Calculate albedo
 	#uav = uav.isel(x=slice(3000,3500),y=slice(3000,3500))
-	albedo = 0.726*(uav['Band2']-0.18) - 0.322*(uav['Band2']-0.18)**2 - 0.015*(uav['Band4']-0.2) + 0.581*(uav['Band4']-0.2)
+	#albedo = 0.726*(uav['Band2']-0.18) - 0.322*(uav['Band2']-0.18)**2 - 0.015*(uav['Band4']-0.2) + 0.581*(uav['Band4']-0.2)
+	albedo = 0.726*uav['Band2'] - 0.322*uav['Band2']**2 - 0.015*uav['Band4'] + 0.581*uav['Band4']
 
 	# Save outputs
 
 	if not setup:
-		## Define projection
+		# Define projection
 		srs = osr.SpatialReference()
 		srs.ImportFromProj4('+init=epsg:32623')
 		crs = xr.DataArray(0, encoding={'dtype':np.dtype('int8')})
 		crs.attrs['projected_crs_name'] = srs.GetAttrValue('projcs')
-		crs.attrs['grid_mapping_name'] = 'UTM'
+		crs.attrs['grid_mapping_name'] = 'universal_transverse_mercator'
 		crs.attrs['scale_factor_at_central_origin'] = srs.GetProjParm('scale_factor')
 		crs.attrs['standard_parallel'] = srs.GetProjParm('latitude_of_origin')
 		crs.attrs['straight_vertical_longitude_from_pole'] = srs.GetProjParm('central_meridian')
@@ -70,23 +88,22 @@ for flight in flights:
 		crs.attrs['latitude_of_projection_origin'] = srs.GetProjParm('latitude_of_origin')
 
 		## Create associated lat/lon coordinates DataArrays
-		uav = georaster.SingleBandRaster('NETCDF:"%s%s":Band1' %(fn_path, flights[flight]),
+		uav_gr = georaster.SingleBandRaster('NETCDF:"%s%s":Band1' %(fn_path, flights[flight]),
 			load_data=False)
-		grid_lon, grid_lat = uav.coordinates(latlon=True)
-		uav = None
+		grid_lon, grid_lat = uav_gr.coordinates(latlon=True)
 		uav = xr.open_dataset(fn_path + flights[flight],chunks={'x':1000,'y':1000}) 
 		coords_geo = {'y': uav['y'], 'x': uav['x']}
 		uav = None
 
 		lon_da = xr.DataArray(grid_lon, coords=coords_geo, dims=['y', 'x'], 
 			encoding={'_FillValue': -9999., 'dtype':'int16', 'scale_factor':0.000000001})
-		lon_da.attrs['grid_mapping'] = 'UTM'
+		lon_da.attrs['grid_mapping'] = 'universal_transverse_mercator'
 		lon_da.attrs['units'] = 'degrees'
 		lon_da.attrs['standard_name'] = 'longitude'
 
 		lat_da = xr.DataArray(grid_lat, coords=coords_geo, dims=['y', 'x'], 
 			encoding={'_FillValue': -9999., 'dtype':'int16', 'scale_factor':0.000000001})
-		lat_da.attrs['grid_mapping'] = 'UTM'
+		lat_da.attrs['grid_mapping'] = 'universal_transverse_mercator'
 		lat_da.attrs['units'] = 'degrees'
 		lat_da.attrs['standard_name'] = 'latitude'
 
@@ -97,18 +114,20 @@ for flight in flights:
 	predicted.attrs['long_name'] = 'Surface classification using Random Forests 6-class classifier'
 	predicted.attrs['units'] = 'None'
 	predicted.attrs['key'] = 'Unknown:0; Water:1; Snow:2; Clean Ice:3; Light Algae on Ice:4; Heavy Algae on Ice:5; Cryoconite:6' ## FIX !!
-	predicted.attrs['grid_mapping'] = 'UTM'
+	predicted.attrs['grid_mapping'] = 'universal_transverse_mercator'
 
 	albedo.encoding = {'dtype':'int16', 'scale_factor':0.01, 'zlib':True, '_FillValue':-9999}
 	albedo.name = 'Surface albedo computed after Knap et al. (1999) narrowband-to-broadband conversion'
 	albedo.attrs['units'] = 'dimensionless'
-	albedo.attrs['grid_mapping'] = 'UTM'
+	albedo.attrs['grid_mapping'] = 'universal_transverse_mercator'
 
 	ds = xr.Dataset({'classified':predicted,
 		'albedo':albedo,
-		'UTM':crs,
+		'universal_transverse_mercator':crs,
 		'lon':lon_da,
 		'lat':lat_da})
+
+	ds = ds.transpose('y','x')
 
 	# Main metadata
 	ds.attrs['Conventions'] = 'CF-1.4'
@@ -138,3 +157,16 @@ for flight in flights:
 	ds.to_netcdf('%s%s' %(fn_path, save_fn), format='NetCDF4')
 
 	ds = None
+
+	alb_gr = uav_gr
+	alb_gr.r = np.flipud(albedo.values)
+	save_fn = flights[flight][:-3] + '_albedo.tif'
+	alb_gr.save_geotiff('%s%s' %(fn_path, save_fn))
+
+	clas_gr = uav_gr
+	clas_gr.r = np.flipud(predicted.values)
+	save_fn = flights[flight][:-3] + '_classified.tif'
+	clas_gr.save_geotiff('%s%s' %(fn_path, save_fn))	
+
+	alb_gr = None
+	clas_gr = None
